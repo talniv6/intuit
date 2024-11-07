398c081ff242620c3094a80761de14f486b0b253
Tal Niv <tal.niv@exodigo.ai>
2024-11-08 00:47:23 +0200

    add column annotations in Player

--- src/main/java/com/talniv/intuit/data/Player.java
+++ src/main/java/com/talniv/intuit/data/Player.java
@@ -1,5 +1,6 @@
 package com.talniv.intuit.data;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
@@ -14,27 +15,73 @@ public class Player {
 
     @Id
     private String playerId;
+
+    @Column
     private Integer birthYear;
+
+    @Column
     private Integer birthMonth;
+
+    @Column
     private Integer birthDay;
+
+    @Column
     private String birthCountry;
+
+    @Column
     private String birthState;
+
+    @Column
     private String birthCity;
+
+    @Column
     private Integer deathYear;
+
+    @Column
     private Integer deathMonth;
+
+    @Column
     private Integer deathDay;
+
+    @Column
     private String deathCountry;
+
+    @Column
     private String deathState;
+
+    @Column
     private String deathCity;
+
+    @Column
     private String nameFirst;
+
+    @Column
     private String nameLast;
+
+    @Column
     private String nameGiven;
+
+    @Column
     private Integer weight;
+
+    @Column
     private Integer height;
+
+    @Column
     private String bats;
+
+    @Column
     private String throwsDirection;
+
+    @Column
     private String debut;
+
+    @Column
     private String finalGame;
+
+    @Column
     private String retroId;
+
+    @Column
     private String bbrefID;
 }
